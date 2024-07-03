d6284ad30561b4a4bfccf07f6084a093795b0208
Constantin Doebert <doebert.constantin@gmail.com>
2024-07-03 22:37:12 +0200

    removed api key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from twelvedata import TDClient
 import pandas as pd
 
-td = TDClient(apikey="eba7da3b24104ca594f061cb762cb8da")
+td = TDClient(apikey="")
 
 df = td.time_series(symbol="AAPL", interval="1day", start_date="2018-01-01", end_date="2024-07-01").as_pandas()
