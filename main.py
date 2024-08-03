d3d4b4a78c4dc0fc9ee8555acee4a865022800b5
Constantin Doebert <doebert.constantin@gmail.com>
2024-08-03 23:29:38 +0200

    removed api key

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import numpy as np
 from scipy.stats import norm
 """
-td = TDClient(apikey="eba7da3b24104ca594f061cb762cb8da")
+td = TDClient(apikey="")
 
 df = td.time_series(symbol="AAPL", interval="1day", start_date="2018-01-01", end_date="2024-07-01").as_pandas()
 """
